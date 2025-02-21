e499951724387111dc934111a7d19a47216a614e
madhuri-54321 <mihir25_2010@rediffmail.com>
2025-02-21 20:33:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def extract_features(image):
     return np.hstack([cnn_feature, glcm_feature])
 
 # Corrected dataset path
-dataset_path = r"mri-prediction-app/moderateDem1.jpg"
+dataset_path = r"mri-prediction-app/ModerateDemented"
 
 # Ensure dataset path exists
 if not os.path.exists(dataset_path):
